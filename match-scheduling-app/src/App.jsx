5ab368ffb2b54c4f564b5df72de2cdbd07c88986
SHADMAN KHAN <khanshadman7777@gmail.com>
2024-08-11 18:15:24 +0530

    Made the structure ready for the app

--- match-scheduling-app/src/App.jsx
+++ match-scheduling-app/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <>
       <h1>Match Scheduling App</h1>
+      <Navbar />
     </>
   );
 }
---
+++ match-scheduling-app/src/Components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <div>
+      <h1>This is Navbar</h1>
+    </div>
+  );
+}
